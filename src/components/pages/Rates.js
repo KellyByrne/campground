f2c192a590efefacdf53f789ec1eb17f839abb71
Darrin Bennett <boristotle@hotmail.com>
2022-07-01 14:45:06 -0400

    rates info

--- src/components/pages/Rates.js
+++ src/components/pages/Rates.js
@@ -3,7 +3,22 @@ import React from 'react';
 class Rates extends React.Component {
     render() {
         return (
-            <div>Rates</div>
+            <div>
+                <h1>Rates</h1>
+                <h3>RV Sites</h3>
+                Sun - Wednesday
+                    1 AC: 50
+                    2 ACs: 60
+                    3+ ACs: 70
+
+                Thursday, Friday, Saturday
+                    1 AC: 60
+                    2 ACs: 70
+                    3+ ACs: 80
+
+                <h3>Tent Sites</h3>
+                $30/night
+            </div>
         );
     };
 }
